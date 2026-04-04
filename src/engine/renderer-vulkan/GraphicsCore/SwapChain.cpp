f7028fe9590e9b9f7a12697e95ed8490d0eac2e2
Reaper <reaper9977@gmail.com>
2026-04-05 02:39:44 +0300

    DynamicArray: size->memorySize, elements->size

--- src/engine/renderer-vulkan/GraphicsCore/SwapChain.cpp
+++ src/engine/renderer-vulkan/GraphicsCore/SwapChain.cpp
@@ -259,7 +259,7 @@ void SwapChain::Init( const VkInstance instance ) {
 
 	res = vkGetSwapchainImagesKHR( device, swapChain, &imageCount, swapchainImages.memory );
 
-	for ( uint32 i = 0; i < images.elements; i++ ) {
+	for ( uint32 i = 0; i < images.size; i++ ) {
 		images[i].Init( swapchainImages[i], format );
 
 		VkSemaphoreTypeCreateInfo semaphoreTypeInfo {
